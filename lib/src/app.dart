96fd8378c7daf06d739124cfa84b22ac8badf312
Abdel-rahmanalaa <Abdelrahmanalaa9911.com>
2023-05-14 01:30:40 +0200

    app file

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
