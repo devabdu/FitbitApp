dbbc595a22d94ee282c61facc509e79134d5c17a
Abdel-rahmanalaa <Abdelrahmanalaa9911.com>
2023-05-14 01:31:03 +0200

    add primary colors

---
+++ lib/src/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color lightPrimaryColor = Color(0xFF499395);
+  static const Color darkPrimaryColor = Color(0xFF174A51);
+}
+
+class AppColorsWithOpacity {}
