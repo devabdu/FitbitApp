94f52e1838516923d3d3a05ca434e8f6c88c7b5f
Abdel-rahmanalaa <abdellrahmanalaa@gmail.com>
2023-07-25 09:32:54 +0200

    add flutter_screenutil pckg and Initialized in app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:fitbit/src/config/routes/app_routes.dart';
 import 'package:fitbit/src/features/auth/presentation/screens/onboarding/onboarding_screen1.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'config/theme/light_theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -10,14 +11,24 @@ class MyApp extends StatelessWidget {
 
   factory MyApp() => _instance;
 
+  static const Size defaultSize = Size(360, 690);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appLightTheme(),
-      home: const OnBoardingScreen1(),
-      onGenerateRoute: AppRoutesGenerator.generatePageRoute,
-      initialRoute: AppRoutesName.onBoarding_1Route,
+    return ScreenUtilInit(
+      designSize: defaultSize,
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: appLightTheme(),
+          home: child,
+          onGenerateRoute: AppRoutesGenerator.generatePageRoute,
+          initialRoute: AppRoutesName.onBoarding_1Route,
+        );
+      },
+      child: const OnBoardingScreen1(),
     );
   }
 }
