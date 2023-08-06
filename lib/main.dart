70bca6587e76e70b5bbff55e3b2659fa9d4c24eb
Abdel-rahmanalaa <abdellrahmanalaa@gmail.com>
2023-08-06 08:19:21 +0200

    Initialize Firebase and add plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fitbit/src/app.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MyApp(),
   );
